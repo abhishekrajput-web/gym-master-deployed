956657b22076662a6f43d1ce85c8a3a17ed67db3
Abhishek Rajput <abhishekrajputcr7@gmail.com>
2025-10-21 18:00:52 +0530

    update the connection db

--- server/utils/connectDB.js
+++ server/utils/connectDB.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = (url) => {
-    mongoose.connect(url).then(() => console.log("connected mongodb successfully")).catch((err) => console.log(err || "mongodb not connected") );
+    mongoose.connect(url).then(() => console.log("connected mongodb successfully")).catch((err) => console.log(err || "Mongodb Not Connected") );
 }
 
 export default connectDB;
